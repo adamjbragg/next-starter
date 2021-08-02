fac2c4597add08d83effa23f20aaa0cf3206065e
Adam Bragg <mail@adamjbragg.net>
2021-08-03 09:01:07 +1000

    add more details to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,17 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import Link from 'next/link';
+
 const Footer = () => (
-  <footer className="flex items-center justify-center w-full h-24 px-5">
-    <p>This is the footer</p>
+  <footer className="flex flex-row items-center justify-between w-full h-24 px-5">
+    <p className="text-sm">{new Date().getFullYear()} © AJB Next Starter</p>
+    <p className="text-sm">
+      Website by:{' '}
+      <Link href="https://adamjbragg.net/">
+        <a rel="noreferrer" target="_blank">
+          Adam J Bragg
+        </a>
+      </Link>
+    </p>
   </footer>
 );
 
