d07162921db8f860ac817e26f161148f0c17b0e1
Adam Bragg <mail@adamjbragg.net>
2021-08-03 08:51:39 +1000

    add padding to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <footer className="flex items-center justify-center w-full h-24">
+  <footer className="flex items-center justify-center w-full h-24 px-5">
     <p>This is the footer</p>
   </footer>
 );
